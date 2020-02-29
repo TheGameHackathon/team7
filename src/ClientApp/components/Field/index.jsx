8015dcf6215932766cadf9a772a835df963c7724
Shcherbina99 <ej99@example.com>
2020-02-29 13:48:08 +0500

    add field

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -4,7 +4,47 @@ import styles from './styles.css'
 export default class Field extends React.Component {
     render () {
         return (
-            <div className={ styles.root }/>
+            <div className={styles.center}>
+                <div className={styles.fieldWrapper}>
+                    <table className={styles.field}>
+                        <tr>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                            <td className={styles.color1}></td>
+                            <td className="color3"></td>
+                            <td className="color4"></td>
+                        </tr>
+                        <tr>
+                            <td className="color4"></td>
+                            <td className="color4"></td>
+                            <td className="color4"></td>
+                            <td className="color1"></td>
+                            <td className="color5"></td>
+                        </tr>
+                        <tr>
+                            <td className="color1"></td>
+                            <td className="color5"></td>
+                            <td className="color1"></td>
+                            <td className="color1"></td>
+                            <td className="color2"></td>
+                        </tr>
+                        <tr>
+                            <td className={styles.color1}></td>
+                            <td className="color2"></td>
+                            <td className="color1"></td>
+                            <td className="color1"></td>
+                            <td className="color3"></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         );
     }
 }
