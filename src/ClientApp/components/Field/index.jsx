863870d8d0919ae0aa14ae79d12e43b4fc8f25a5
CHAMPIGNOM <orizona-35@inbox.ru>
2020-02-29 14:17:19 +0500

    fix field

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -6,7 +6,7 @@ export default class Field extends React.Component {
         super();
         this.state = {
             colors: [styles.color1, styles.color2, styles.color3, styles.color4, styles.color5],
-            field: [[4]]
+            field: [[], [],[],[],[]]
         }
     }
 
@@ -25,32 +25,32 @@ export default class Field extends React.Component {
                             <td className={this.colors[this.state.field[0][4]]}></td>
                         </tr>
                         <tr>
-                            <td className={this.colors[this.state.field[0][0]]}></td>
-                            <td className={this.colors[this.state.field[0][1]]}></td>
-                            <td className={this.colors[this.state.field[0][2]]}></td>
-                            <td className={this.colors[this.state.field[0][3]]}></td>
-                            <td className={this.colors[this.state.field[0][4]]}></td>
+                            <td className={this.colors[this.state.field[1][0]]}></td>
+                            <td className={this.colors[this.state.field[1][1]]}></td>
+                            <td className={this.colors[this.state.field[1][2]]}></td>
+                            <td className={this.colors[this.state.field[1][3]]}></td>
+                            <td className={this.colors[this.state.field[1][4]]}></td>
                         </tr>
                         <tr>
-                            <td className={this.colors[this.state.field[0][0]]}></td>
-                            <td className={this.colors[this.state.field[0][1]]}></td>
-                            <td className={this.colors[this.state.field[0][2]]}></td>
-                            <td className={this.colors[this.state.field[0][3]]}></td>
-                            <td className={this.colors[this.state.field[0][4]]}></td>
+                            <td className={this.colors[this.state.field[2][0]]}></td>
+                            <td className={this.colors[this.state.field[2][1]]}></td>
+                            <td className={this.colors[this.state.field[2][2]]}></td>
+                            <td className={this.colors[this.state.field[2][3]]}></td>
+                            <td className={this.colors[this.state.field[2][4]]}></td>
                         </tr>
                         <tr>
-                            <td className={this.colors[this.state.field[0][0]]}></td>
-                            <td className={this.colors[this.state.field[0][1]]}></td>
-                            <td className={this.colors[this.state.field[0][2]]}></td>
-                            <td className={this.colors[this.state.field[0][3]]}></td>
-                            <td className={this.colors[this.state.field[0][4]]}></td>
+                            <td className={this.colors[this.state.field[3][0]]}></td>
+                            <td className={this.colors[this.state.field[3][1]]}></td>
+                            <td className={this.colors[this.state.field[3][2]]}></td>
+                            <td className={this.colors[this.state.field[3][3]]}></td>
+                            <td className={this.colors[this.state.field[3][4]]}></td>
                         </tr>
                         <tr>
-                            <td className={this.colors[this.state.field[0][0]]}></td>
-                            <td className={this.colors[this.state.field[0][1]]}></td>
-                            <td className={this.colors[this.state.field[0][2]]}></td>
-                            <td className={this.colors[this.state.field[0][3]]}></td>
-                            <td className={this.colors[this.state.field[0][4]]}></td>
+                            <td className={this.colors[this.state.field[4][0]]}></td>
+                            <td className={this.colors[this.state.field[4][1]]}></td>
+                            <td className={this.colors[this.state.field[4][2]]}></td>
+                            <td className={this.colors[this.state.field[4][3]]}></td>
+                            <td className={this.colors[this.state.field[4][4]]}></td>
                         </tr>
                     </table>
                 </div>
