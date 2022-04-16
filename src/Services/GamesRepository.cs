36c8ac7c4ffe12c71c04c810b3e84c31d7f96c20
PavelLediankin <a.lediankin@yandex.ru>
2022-04-16 13:02:32 +0500

    parameters for GameDTO

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -19,25 +19,25 @@ namespace thegame.Services
             return gameDto;
         }
 
-        private static GameDto CreateGameDto()
+        private static GameDto CreateGameDto(int width = 8, int height = 8, int colorsCount = 5)
         {
-            var width = 10;
-            var height = 8;
+            if (colorsCount > 5)
+                colorsCount = 5;
             var testCells = new CellDto[width * height];
             var random = new Random();
-            
+
             for (var i = 0; i < height; i++)
             {
                 for (var j = 0; j < width; j++)
                 {
-                    var color = random.Next(1, 5);
+                    var color = random.Next(1, colorsCount);
                     testCells[i * height + j] = new CellDto($"{i * width + j}",
                         new VectorDto(i, j), $"color{color}", "", 0);
                 }
             }
-            
-            return new GameDto(testCells, true, 
-                true, width, height, Guid.Empty, 
+
+            return new GameDto(testCells, true,
+                true, width, height, Guid.Empty,
                 false, 0);
         }
     }
