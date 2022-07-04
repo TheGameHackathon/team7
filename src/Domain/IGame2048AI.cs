73ec9ce59ea8c353711e39732eafb24957cf4ef8
Sergei <sergei.lugovykh@yandex.ru>
2022-07-04 16:32:11 +0500

    Add ai interface

---
+++ src/Domain/IGame2048AI.cs
@@ -0,0 +1,8 @@
+﻿using thegame.Domain.Models;
+
+namespace thegame.Domain;
+
+public interface IGame2048AI
+{
+    UserMove ComputeMove(Game game);
+}
---
+++ src/Domain/Random2048Ai.cs
@@ -0,0 +1,11 @@
+﻿using thegame.Domain.Models;
+
+namespace thegame.Domain;
+
+public class Random2048Ai : IGame2048AI
+{
+    public UserMove ComputeMove(Game game)
+    {
+        throw new System.NotImplementedException();
+    }
+}
