de3f88a4825e4c8799881bb722b09c498636d733
Ivan Sychev <funovichf@gmail.com>
2022-07-04 12:40:52 +0500

    add moves method

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -1,20 +1,45 @@
 using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using thegame.Domain;
 using thegame.Models;
 using thegame.Services;
+using AutoMapper;
+using thegame.Domain.Models;
 
 namespace thegame.Controllers;
 
+[ApiController]
 [Route("api/games/{gameId}/moves")]
 public class MovesController : Controller
 {
-    [HttpPost]
-    public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInput)
+    private readonly IGameRepository gameRepository;
+    private readonly IMapper mapper;
+    private readonly IGame2048Handler game2048Handler;
+        
+    public MovesController(IGameRepository gameRepository, IMapper mapper, IGame2048Handler game2048Handler)
     {
-        var game = TestData.AGameDto(userInput.ClickedPos ?? new VectorDto {X = 1, Y = 1});
-        if (userInput.ClickedPos != null)
-            game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;
-        return Ok(game);
+        this.gameRepository = gameRepository;
+        this.mapper = mapper;
+        this.game2048Handler = game2048Handler;
+    }
+        
+    [HttpPost]
+    public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInputDto)
+    {
+        if (userInputDto == null || gameId == Guid.Empty)
+            return BadRequest();
+
+        var userMove = mapper.Map<UserMove>(userInputDto);
+        var game = gameRepository.GetGame(gameId);
+        game = game2048Handler.MakeMove(game, userMove);
+        gameRepository.Update(game);
+
+        var gameDto = mapper.Map<GameDto>(game);
+        
+        return CreatedAtRoute(
+            nameof(Moves),
+            new { gameId },
+            gameDto);
     }
 }
