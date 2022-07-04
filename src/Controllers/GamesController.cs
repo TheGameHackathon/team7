4a42355ccae61d20c559f615495038df08e9959d
Ruslan Vlasenko <ruslanvlasenko1488@gmail.com>
2022-07-04 12:26:15 +0500

    Add Index method

--- src/Controllers/GamesController.cs
+++ src/Controllers/GamesController.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using thegame.Domain;
+using thegame.Domain.Models;
 using thegame.Models;
 using thegame.Services;
 
@@ -7,9 +10,26 @@ namespace thegame.Controllers;
 [Route("api/games")]
 public class GamesController : Controller
 {
+    private readonly IGameRepository gameRepository;
+    private readonly IGame2048Handler game2048Handler;
+    private readonly IMapper mapper;
+
+    public GamesController(IGame2048Handler game2048Handler, IGameRepository gameRepository, IMapper mapper)
+    {
+        this.game2048Handler = game2048Handler;
+        this.gameRepository = gameRepository;
+        this.mapper = mapper;
+    }
+    
     [HttpPost]
     public IActionResult Index()
     {
-        return Ok(TestData.AGameDto(new VectorDto {X = 1, Y = 1}));
+        var game = game2048Handler.StartGame();
+
+        gameRepository.AddGame(game);
+
+        var gameDto = mapper.Map<GameDto>(game);
+
+        return Ok(gameDto);
     }
 }
