2b5df61b8da0900f6bc645015fb3c92f7a65f02c
Ruslan Vlasenko <ruslanvlasenko1488@gmail.com>
2022-07-04 13:16:12 +0500

    Add Mapper class

---
+++ src/Domain/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using thegame.Domain.Models;
+using thegame.Models;
+
+namespace thegame.Domain;
+
+public class Mapper
+{
+    public UserMove MapFromUserInputDtoToUserMove(UserInputDto userInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GameDto MapFromGameToGameDto(Game game)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CellDto MapFromCellToCellDto(Cell cell)
+    {
+        throw new NotImplementedException();
+    }
+}
